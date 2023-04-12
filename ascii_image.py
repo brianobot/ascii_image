28cf2b1b6a287a53acafbc17ec8c91f6e40f3020
Brian Obot <brianobot9@gmail.com>
2023-04-12 17:39:25 +0100

    Update script function documentaion and add requirement file

--- ascii_image.py
+++ ascii_image.py
@@ -2,26 +2,34 @@ import sys
 import PIL.Image
 from datetime import datetime
 
+
 ASCII_CHARS = ["@", "#", '$', "%", "&", "?", "+",";", ":", ",", "."]
 
+
 def resize_image(image, new_width=100):
+    """function to resize an image object."""
     width, height = image.size
     ratio = height/width
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
     return resized_image
 
+
 def grayify(image):
+    """function to convert an image obj into grayscale coloration."""
     grayscale_image = image.convert("L")
     return grayscale_image
 
+
 def pixel_to_ascii(image):
+    """function to convert pixel to ascii characters"""
     pixels = image.getdata()
     characters = "".join(ASCII_CHARS[pixel//25] for pixel in pixels)
     return characters
 
 
 def main(new_width=100):
+    """Main function starts program execution."""
     if len(sys.argv) > 1:
         files = sys.argv[1:]
     else:
